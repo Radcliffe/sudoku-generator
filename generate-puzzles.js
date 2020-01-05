8da1586bbdc462e3123c11f0b2cca7c78d55bc3f
David Radcliffe <dradcliffe@gmail.com>
2020-01-04 19:19:37 -0600

    refactor

--- generate-puzzles.js
+++ generate-puzzles.js
@@ -4,64 +4,9 @@ var mysql = require('mysql');
 var secrets = require('./secrets');
 var connection = mysql.createConnection(secrets.db);
 
-function stringify(puzzle) {
-    return puzzle.map(x => {
-        return x === null ? '.' : x + 1;
-    }).join('');
-}
-
-function countClues(puzzle) {
-    return puzzle.filter(x => {
-        return x !== null;
-    }).length;
-}
-
-function createPuzzle() {
-    const puzzle = sudoku.makepuzzle();
-    const solution = sudoku.solvepuzzle(puzzle);
-    const difficulty = sudoku.ratepuzzle(puzzle, 10);
-    const clues = countClues(puzzle);
-    return {
-        puzzle: stringify(puzzle),
-        solution: stringify(solution),
-        clues: clues,
-        difficulty: difficulty,
-    }
-}
-
-function savePuzzle(p) {
-    connection.query('INSERT INTO sudoku (puzzle, solution, clues, difficulty) VALUES (?, ?, ?, ?)',
-        [p.puzzle, p.solution, p.clues, p.difficulty],
-        function (error, results, fields) {
-            if (error) throw error;
-        }
-    );
-}
-
-function generatePuzzles(n, batchSize = 1000) {
-    batchSize = Math.min(batchSize, n);
-    console.log(`Puzzles remaining: ${n}`)
-    connection.beginTransaction(function (err) {
-        for (let i = 0; i < batchSize; i++) {
-            let p = createPuzzle();
-            savePuzzle(p);
-        }
-        connection.commit(function (err) {
-            if (err) {
-                connection.rollback(function () {
-                    throw err;
-                });
-            }
-            if (n > batchSize) {
-                generatePuzzles(n - batchSize, batchSize);
-            } else {
-                connection.end();
-            }
-        });
-    });
-}
+var g = require('./index.js');
 
 var args = process.argv.slice(2);
 var numberOfPuzzles = args[0] ? +args[0] : 1000000;
 var batchSize = args[1] ? +args[1] : 1000;
-generatePuzzles(numberOfPuzzles, batchSize);
+g.generatePuzzles(numberOfPuzzles, batchSize);
